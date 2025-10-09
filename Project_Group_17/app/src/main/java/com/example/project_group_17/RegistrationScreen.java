f0ec2f59cac0bf9f799965803d88752dc29e5496
LucasSteele71 <114436337+LucasSteele71@users.noreply.github.com>
2025-10-09 19:49:58 -0400

    Implemented switching between the different screens neccesary for registration next will implement the login page functionality

---
+++ Project_Group_17/app/src/main/java/com/example/project_group_17/RegistrationScreen.java
@@ -0,0 +1,36 @@
+package com.example.project_group_17;
+
+import static com.example.project_group_17.R.*;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistrationScreen extends AppCompatActivity {
+    Button btnGoStudent;
+    Button btnGoTutor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration);
+
+        btnGoStudent= findViewById(id.btnGoStudent);
+        btnGoTutor= findViewById(R.id.btnGoTutor);
+
+
+
+        // Start new Activity with explicit Intent
+        btnGoStudent.setOnClickListener(v -> {
+            Intent intent = new Intent(RegistrationScreen.this, StudentRegScreen.class);
+            startActivity(intent);
+        });
+        // Start new Activity with explicit Intent
+        btnGoTutor.setOnClickListener(v -> {
+            Intent intent = new Intent(RegistrationScreen.this, TutorRegScreen.class);
+            startActivity(intent);
+        });
+    }
+}
---
+++ Project_Group_17/app/src/main/java/com/example/project_group_17/TutorRegScreen.java
@@ -0,0 +1,6 @@
+package com.example.project_group_17;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TutorRegScreen extends AppCompatActivity {
+}
