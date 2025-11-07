c17f34daafba9d1c3de03ffe53732693a2f74880
Olivier Currie <olivier_currie@icloud.com>
2025-11-07 17:28:58 -0500

    Created logic for the tutor to access the schedule creation page from the user screen

--- Project_Group_17/app/src/main/java/com/example/project_group_17/Screens/UserScreen.java
+++ Project_Group_17/app/src/main/java/com/example/project_group_17/Screens/UserScreen.java
@@ -3,6 +3,7 @@ package com.example.project_group_17.Screens;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,9 +15,13 @@ import com.example.project_group_17.UserHierarchy.User;
 import java.io.Serializable;
 
 public class UserScreen extends AppCompatActivity {
+
+    Button continueBtn;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.userscreen_main);
+        continueBtn = findViewById(R.id.cnt);
         Serializable admin = getIntent().getSerializableExtra("isAdmin");
         if(admin != null){
             Admin a = (Admin) admin;
@@ -28,6 +33,14 @@ public class UserScreen extends AppCompatActivity {
             TextView userType =  findViewById(R.id.userTypeTextViewId);
             assert u != null;
             userType.setText(u.getUserType());
+            if (u.getUserType().equals("Tutor")) {
+                continueBtn.setText("Create Schedule");
+                continueBtn.setOnClickListener(v -> {
+                    Intent intent = new Intent(UserScreen.this, TutorCreatingSlots.class);
+                    startActivity(intent);
+                    finish();
+                });
+            }
         }
 
     }
