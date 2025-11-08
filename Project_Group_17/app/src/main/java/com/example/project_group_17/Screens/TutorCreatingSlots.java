648be92f236c1c62640649ef183fa55f29e5fa00
Omar El Kady <oelka080@uottawa.ca>
2025-11-07 19:09:57 -0500

    Added TutorCreatingSlots class

--- Project_Group_17/app/src/main/java/com/example/project_group_17/Screens/TutorCreatingSlots.java
+++ Project_Group_17/app/src/main/java/com/example/project_group_17/Screens/TutorCreatingSlots.java
@@ -1,4 +1,56 @@
 package com.example.project_group_17.Screens;
 
-public class TutorCreatingSlots {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.project_group_17.R;
+import com.example.project_group_17.TutorFunctions.Schedule;
+import com.example.project_group_17.TutorFunctions.TimeSlot;
+
+import java.util.List;
+
+public class TutorCreatingSlots extends AppCompatActivity {
+
+    private EditText date;
+    private EditText startTime;
+    private EditText endTime;
+    private CheckBox isAutoApporved;
+    private Button create;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.schedule);
+
+        date = findViewById(R.id.etDate);
+        startTime = findViewById(R.id.etStartTime);
+        endTime = findViewById(R.id.etEndTime);
+        isAutoApporved = findViewById(R.id.cbAutoApprove);
+        create = findViewById(R.id.btnCreate);
+
+        create.setOnClickListener(v -> generateSchedule());
+
+    }
+
+    private void generateSchedule() {
+
+        String d = date.getText().toString().trim();
+        String start = startTime.getText().toString().trim();
+        String end = endTime.getText().toString().trim();
+        boolean auto = isAutoApporved.isChecked();
+
+        List<TimeSlot> slots = Schedule.incrSlots(start, end);
+
+        Schedule schedule = new Schedule("FETCH FROM FIREBASE",d,auto,slots);
+
+        //NEED TO FIND A WAY TO STORE SCHEDULE IN FIREBASE.
+
+
+    }
+
 }
+
